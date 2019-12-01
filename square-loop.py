17dd58d5711256773f018be2ec4615be9718f0d8
Andrew Kleindolph <akleindolph@gmail.com>
2019-12-01 14:44:42 -0800

    Update square-loop.py

--- square-loop.py
+++ square-loop.py
@@ -1,16 +1,22 @@
-# CircuitPython demo - NeoPixel
 import time
 import board
 import neopixel
 import random
 import digitalio
 import adafruit_dotstar
+from analogio import AnalogIn
 
 pixel_pin = board.A1
+
+seedPin = AnalogIn(board.A2)
+randnum = seedPin.value
+random.seed(randnum)
+
 num_pixels = 47
-randnum = 0
 grow = 0
 decend = 0
+
+print("start", randnum)
 
 builtin = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
 builtin[0] = (0, 0, 2)
@@ -20,18 +26,18 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.8, auto_write=Fal
 def color_chase(color, wait):
     grow = random.randint(0, 46)
     decend = grow
-    print("grow point",grow)
-    for i in range(grow,num_pixels):
+    print("grow point", grow)
+    for i in range(grow, num_pixels):
         if decend > 0:
             decend -= 1
         pixels[i] = color
         pixels[decend] = color
-        print("decend",decend,"i",i)
+        #print("decend", decend, "i", i)
         time.sleep(wait)
         pixels.show()
-    for decend in range(decend,0,-1):
+    for decend in range(decend, -1, -1):
         pixels[decend] = color
-        print("decendcon",decend,"i",i)
+        #print("decendcon", decend, "i", i)
         time.sleep(wait)
         pixels.show()
     time.sleep(900.0)
@@ -45,10 +51,11 @@ BLUISH = (150, 180, 200)
 COLDDAY = (200, 130, 255)
 NEWWHITE = (200, 250, 100)
 BRIGHTYELLOWYGREEN = (200, 255, 30)
+UNEXPECTED = (random.randint(0,255),255,random.randint(0,255))
 
 while True:
-    randnum = random.randint(0, 8)
-    print("color",randnum)
+    randnum = random.randint(0, 9)
+    print("color", randnum)
     if randnum == 0:
         color_chase(CYANNY, 0.1)  # Increase the num to slow down color
     if randnum == 1:
@@ -67,3 +74,5 @@ while True:
         color_chase(NEWWHITE, 0.1)
     if randnum == 8:
         color_chase(BRIGHTYELLOWYGREEN, 0.1)
+    if randnum == 9:
+        color_chase(UNEXPECTED, 0.1)
